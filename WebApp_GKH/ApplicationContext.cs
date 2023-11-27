393b373ec1efd4f6638e3f27b6b0bb511e18a235
KristinaSunik <anitanit43@gmail.com>
2023-11-27 17:15:22 +0300

    Подключение БД

---
+++ WebApp_GKH/ApplicationContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Hosting.Server;
+using Microsoft.EntityFrameworkCore;
+
+public class ApplicationContext : DbContext
+{
+   // public DbSet<User> Users { get; set; } = null!;
+
+    public ApplicationContext()
+    {
+        Database.EnsureCreated();
+    }
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseNpgsql("Host=45.143.93.41;Port=5432;Database=db_edu;Username=edu;Password=edu");
+    }
+}
