a252e60f56577a74d7b94507592e3ea372acbdc7
KristinaSunik <anitanit43@gmail.com>
2023-12-18 19:19:39 +0300

    вернулся Сваггер

--- WebApp_GKH/Program.cs
+++ WebApp_GKH/Program.cs
@@ -4,40 +4,26 @@ var builder = WebApplication.CreateBuilder(args);
 // add context DatabaseContext as a service
 builder.Services.AddDbContext<ApplicationDbContext>();
 
-builder.Services.AddControllersWithViews();
 // Add services to the container.
 builder.Services.AddControllers();
 
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-//builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseDeveloperExceptionPage();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
-else
-{
-    app.UseExceptionHandler("/Home/Error");
-   
-    app.UseHsts();
-}
-
 
 app.UseHttpsRedirection();
-app.UseStaticFiles();
 
-app.MapControllers();
-app.UseRouting();
 app.UseAuthorization();
 
-app.UseEndpoints(endpoints =>
-{
-    endpoints.MapControllerRoute(
-                    name: "default",
-                    pattern: "{controller=Home}/{action=Index}");
-});
+app.MapControllers();
 
 app.Run();
